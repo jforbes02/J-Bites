20d922b35731c3075ea7fabf42a4f5de7e40b391
forbesCoding <131290572+jforbes02@users.noreply.github.com>
2025-11-21 20:51:38 -0500

    created reviews and pydantic user model

--- Database/dbModels.py
+++ Database/dbModels.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, Enum as SQLEnum, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from .dbConnect import Base
 from enum import Enum
 
@@ -42,6 +42,7 @@ class StatusName(str, Enum):
 class OrderResponse(BaseModel):
     id: int
     status: StatusName
+    phone_num: str | None
 
 class Order(Base):
     __tablename__ = "orders"
@@ -49,16 +50,23 @@ class Order(Base):
     status = Column(SQLEnum(StatusName), nullable=False)
     user_id = Column(Integer, ForeignKey("users.user_id"))
     user = relationship("User", back_populates="orders")
-
+    phone_num = Column(String, index=True)
     def __repr__(self):
         return f"<Order(item='{self.id}', user_id='{self.user_id}')>"
 
+
 #Users
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    password: str
+
 class User(Base):
     __tablename__ = "users"
     user_id = Column(Integer, primary_key=True)
     name = Column(String, index=True)
-    email = Column(String, index=True)
+    email = Column(EmailStr, index=True)
     password = Column(String) #TODO: Ethan work on security measures
 
     orders = relationship("Order", back_populates="user")
@@ -72,3 +80,18 @@ class Admin(Base):
     username = Column(String, index=True)
     password = Column(String) #TODO: Ethan work on security measures
     is_admin = Column(Boolean, default=True)
+
+#Reviews
+class ReviewResponse(BaseModel):
+    id: int
+    reviewer_id: int
+    reviewer_name: str
+    rating: int
+
+class Review(Base):
+    __tablename__ = "reviews"
+    id = Column(Integer, primary_key=True)
+    reviewer_id = Column(Integer, ForeignKey("users.user_id"))
+    reviewer_name = Column(String)
+    rating = Column(Integer)
+    reviewer = relationship("User", back_populates="reviews")
