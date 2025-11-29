2611fd07e663c441928356e43a99ea8201c0050c
forbesCoding <131290572+jforbes02@users.noreply.github.com>
2025-11-28 19:43:49 -0500

    Order Cancellation and Getting orders by phone#

--- main.py
+++ main.py
@@ -141,5 +141,63 @@ def get_order(order_id: int, session: dbSession):
 
     )
 
+@app.post("/orders/{order_id}/cancel")
+def cancel_order(order_id: int, session: dbSession):
+    order = session.query(Order).filter(Order.id == order_id).first()
+    if not order:
+        raise HTTPException(status_code=404, detail="Order not found")
+
+    if order.status == OrderStatus.PENDING:
+        raise HTTPException(status_code=400, detail="Cannot cancel completed order. Please contact support for refunds")
+    if order.status == OrderStatus.DONE:
+        raise HTTPException(
+            status_code=400,
+            detail="Cannot cancel completed order. Please contact support for refunds."
+        )
+    #TODO add Stripe refund logic
+    try:
+        session.delete(order)
+        session.commit()
+        session.refresh(order)
+        return {"message": "Order canceled.", "order": order.id}
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=500, detail=f"Cancellation Failed: {str(e)}")
+
+@app.get("/orders/search/{phone_num}", response_model=List[OrderResponse])
+def get_order_by_phone(phone_num: str, session: dbSession):
+    orders = session.query(Order).filter(Order.phone_num == phone_num).all()
+    if not orders:
+        raise HTTPException(status_code=404, detail="Order not found")
+    response = []
+
+    for order in orders:
+        items_response = []
+        total_price = 0
+
+        for o_item in order.order_items:
+            items_response.append(
+                OrderItemResponse(
+                    id=o_item.id,
+                    item_id=o_item.item_id,
+                    item_name=o_item.item.name,
+                    quantity=o_item.quantity,
+                    price=o_item.price_at_order
+                )
+            )
+            total_price += o_item.price_at_order * o_item.quantity
+
+        response.append(
+            OrderResponse(
+                id=order.id,
+                status=order.status,
+                phone_num=order.phone_num,
+                items=items_response,
+                total_price=total_price,
+            )
+        )
+
+    return response
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
